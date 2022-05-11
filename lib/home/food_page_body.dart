a02b4351bac73cbcc8cd91831c10320beab63aaf
Daniel <danireysan@gmail.com>
2022-05-11 14:34:28 -0500

    Finished stacked card

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,5 +1,7 @@
 import 'package:ecommerce_app/utils/colors.dart';
 import 'package:ecommerce_app/widgets/big_text.dart';
+import 'package:ecommerce_app/widgets/icon_and_text_widget.dart';
+import 'package:ecommerce_app/widgets/small_text.dart';
 import 'package:flutter/material.dart';
 
 class FoodPageBody extends StatefulWidget {
@@ -11,10 +13,25 @@ class FoodPageBody extends StatefulWidget {
 
 class _FoodPageBodyState extends State<FoodPageBody> {
   final PageController pageController = PageController(viewportFraction: 0.85);
+  var _currPageValue = 0.0;
+  double _scaleFactor = 0.8;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+     _currPageValue =  pageController.page!;
+
+    });
+  }
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
       height: 320,
       child: PageView.builder(
         controller: pageController,
@@ -27,6 +44,8 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
+    // This is going to manage the animation
+    Matrix4 matrix = new Matrix4.identity();
     return Stack(
       children: [
         Container(
@@ -47,34 +66,60 @@ class _FoodPageBodyState extends State<FoodPageBody> {
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
-            height: 140,
-            margin: const EdgeInsets.only(left: 30, right: 30, bottom: 15),
+            height: 120,
+            margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
                 // TODO: make an app theme
                 color: Colors.white),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                BigText(text: "Chinese Side"),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: [
-                    Wrap(
-                      children: List.generate(
+            child: Container(
+              padding: const EdgeInsets.only(right: 15, left: 15, top: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "Taco de Pastor"),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Wrap(
+                          children: List.generate(
                         5,
-                        (index) =>
-                            Icon(Icons.star, color: AppColors.mainColor),
-                      ),
-                    )
-                  ],
-                ),
-                Row(
-                  children: [],
-                )
-              ],
+                        (index) => Icon(
+                          Icons.star,
+                          color: AppColors.mainColor,
+                          size: 15,
+                        ),
+                      )),
+                      // TODO: address the color shade
+                      const SizedBox(width: 10),
+                      SmallText(text: "4.5"),
+                      const SizedBox(width: 10),
+                      SmallText(text: "1729"),
+                      const SizedBox(width: 10),
+                      SmallText(text: "comments"),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      IconAndTextWidget(
+                          icon: Icons.circle_sharp,
+                          text: 'Normal',
+                          iconColor: AppColors.iconColor1),
+                      IconAndTextWidget(
+                          icon: Icons.location_on,
+                          text: '1.7km',
+                          iconColor: AppColors.iconColor2),
+                      IconAndTextWidget(
+                          icon: Icons.access_time_rounded,
+                          text: '23min',
+                          iconColor: Colors.red),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
