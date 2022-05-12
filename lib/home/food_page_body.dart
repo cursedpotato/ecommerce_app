511fdb6788fdc68304a3dafd0db23db3e22ca7cd
Daniel <danireysan@gmail.com>
2022-05-11 19:14:11 -0500

    added dots indicator to food_page_body

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,3 +1,4 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:ecommerce_app/utils/colors.dart';
 import 'package:ecommerce_app/widgets/big_text.dart';
 import 'package:ecommerce_app/widgets/icon_and_text_widget.dart';
@@ -14,116 +15,189 @@ class FoodPageBody extends StatefulWidget {
 class _FoodPageBodyState extends State<FoodPageBody> {
   final PageController pageController = PageController(viewportFraction: 0.85);
   var _currPageValue = 0.0;
-  double _scaleFactor = 0.8;
+  final double _scaleFactor = 0.8;
+  final double _height = 220;
 
   @override
   void initState() {
     super.initState();
     pageController.addListener(() {
-     _currPageValue =  pageController.page!;
-
+      setState(() {
+        _currPageValue = pageController.page!;
+      });
     });
   }
+
   @override
   void dispose() {
     pageController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 320,
-      child: PageView.builder(
-        controller: pageController,
-        itemCount: 5,
-        itemBuilder: (context, position) {
-          return _buildPageItem(position);
-        },
-      ),
+    // This was a container before so let's see how this behaves
+    return Column(
+      children: [
+        SizedBox(
+          height: 320,
+          child: PageView.builder(
+            controller: pageController,
+            itemCount: 5,
+            itemBuilder: (context, position) {
+              return _buildPageItem(position);
+            },
+          ),
+        ),
+        DotsIndicator(
+  dotsCount: 5,
+  position: _currPageValue,
+  decorator: DotsDecorator(
+    activeColor: AppColors.mainColor,
+    size: const Size.square(9.0),
+    activeSize: const Size(18.0, 9.0),
+    activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+  ),
+)
+      ],
+      
     );
   }
 
   Widget _buildPageItem(int index) {
-    // This is going to manage the animation
-    Matrix4 matrix = new Matrix4.identity();
-    return Stack(
-      children: [
-        Container(
-          height: 220,
-          margin: const EdgeInsets.symmetric(horizontal: 5),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            // TODO: make an app theme
-            color: index.isEven
-                ? const Color(0xFF69c5df)
-                : const Color(0xFF9294cc),
-            image: const DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage('assets/images/food01.jpg'),
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 120,
-            margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+    
+    Matrix4 animationMatrix() {
+      // As far as I understand this is an empty matrix of 4 rows and 4 columns
+      Matrix4 matrix = Matrix4.identity();
+      int pageControllerRounded = _currPageValue.floor();
+      isIndexEqPage(int number) => index == pageControllerRounded + number;
+
+      var currScale = 0.8; 
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, _height*(1-_scaleFactor)/2, 1);
+      if (isIndexEqPage(0)) {
+        // This is equation is only true for the current index
+        var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
+        var currTrans = _height * (1 - currScale) / 2;
+        matrix = Matrix4.diagonal3Values(1, currScale, 1)
+          ..setTranslationRaw(0, currTrans, 0);
+      }
+      if (isIndexEqPage(1)) {
+        var currScale =
+            _scaleFactor + (_currPageValue - index + 1) * (1 - _scaleFactor);
+        var currTrans = _height * (1 - currScale) / 2;
+        matrix = Matrix4.diagonal3Values(1, currScale, 1);
+        matrix = Matrix4.diagonal3Values(1, currScale, 1)
+          ..setTranslationRaw(0, currTrans, 0);
+      }
+      if (isIndexEqPage(-1)) {
+        var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
+        var currTrans = _height * (1 - currScale) / 2;
+        matrix = Matrix4.diagonal3Values(1, currScale, 1);
+        matrix = Matrix4.diagonal3Values(1, currScale, 1)
+          ..setTranslationRaw(0, currTrans, 0);
+      }
+
+      return matrix;
+    }
+
+    return Transform(
+      transform: animationMatrix(),
+      child: Stack(
+        children: [
+          Container(
+            height: 220,
+            margin: const EdgeInsets.symmetric(horizontal: 5),
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                // TODO: make an app theme
-                color: Colors.white),
-            child: Container(
-              padding: const EdgeInsets.only(right: 15, left: 15, top: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  BigText(text: "Taco de Pastor"),
-                  const SizedBox(height: 10),
-                  Row(
-                    children: [
-                      Wrap(
-                          children: List.generate(
-                        5,
-                        (index) => Icon(
-                          Icons.star,
-                          color: AppColors.mainColor,
-                          size: 15,
-                        ),
-                      )),
-                      // TODO: address the color shade
-                      const SizedBox(width: 10),
-                      SmallText(text: "4.5"),
-                      const SizedBox(width: 10),
-                      SmallText(text: "1729"),
-                      const SizedBox(width: 10),
-                      SmallText(text: "comments"),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    children: [
-                      IconAndTextWidget(
-                          icon: Icons.circle_sharp,
-                          text: 'Normal',
-                          iconColor: AppColors.iconColor1),
-                      IconAndTextWidget(
-                          icon: Icons.location_on,
-                          text: '1.7km',
-                          iconColor: AppColors.iconColor2),
-                      IconAndTextWidget(
-                          icon: Icons.access_time_rounded,
-                          text: '23min',
-                          iconColor: Colors.red),
-                    ],
-                  )
-                ],
+              borderRadius: BorderRadius.circular(30),
+              // TODO: make an app theme
+              color: index.isEven
+                  ? const Color(0xFF69c5df)
+                  : const Color(0xFF9294cc),
+              image: const DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/images/food01.jpg'),
               ),
             ),
           ),
-        ),
-      ],
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 120,
+              margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                // TODO: make an app theme
+                color: Colors.white,
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color(0xFFe8e8e8),
+                    blurRadius: 5,
+                    offset: Offset(0, 5), // changes position of shadow
+                  ),
+                  // This will cover the left side of the black shadow
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(-5, 0), // changes position of shadow
+                  ),
+                  // This will cover the right side of the black shadow
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(5, 0), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Container(
+                padding: const EdgeInsets.only(right: 15, left: 15, top: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Taco de Pastor"),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Wrap(
+                            children: List.generate(
+                          5,
+                          (index) => Icon(
+                            Icons.star,
+                            color: AppColors.mainColor,
+                            size: 15,
+                          ),
+                        )),
+                        const SizedBox(width: 10),
+                        SmallText(text: "4.5"),
+                        const SizedBox(width: 10),
+                        SmallText(text: "1729"),
+                        const SizedBox(width: 10),
+                        SmallText(text: "comments"),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        IconAndTextWidget(
+                            icon: Icons.circle_sharp,
+                            text: 'Normal',
+                            iconColor: AppColors.iconColor1),
+                        IconAndTextWidget(
+                            icon: Icons.location_on,
+                            text: '1.7km',
+                            iconColor: AppColors.mainColor),
+                        IconAndTextWidget(
+                            icon: Icons.access_time_rounded,
+                            text: '23min',
+                            iconColor: AppColors.iconColor2),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
